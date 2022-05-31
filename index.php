4069a1458e461118e31d9686759527393dddc991
JayBansod <jaybansod678@gmail.com>
2022-05-31 23:38:26 +0530

    web

--- index.php
+++ index.php
@@ -1,174 +1,158 @@
 <?php
-session_start();
-
-
-@include 'config.php';
-      $name_='';
-      if(isset($_GET['nam'])){
-        
-         $name_=$_GET['nam'];
-        
-      }
-      
-
-        
-
-if(isset($_POST['add_to_cart_Main_Course'])){
-   
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-
-   $product_image = $_POST['product_image'];
-   $product_quantity = 1;
-
-   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE (name = '$product_name' AND table_number ='$name_')");
-
-   if(mysqli_num_rows($select_cart) > 10){
-      $message[] = 'product already added to cart';
-   }else{
-      $insert_product = mysqli_query($conn, "INSERT INTO `cart`(name, prise, image, quantity,table_number,is_confirmed) VALUES('$product_name', '$product_price', '$product_image', '$product_quantity','$name_',0)");
-      $message[] = 'product added to cart succesfully';
-   }
-
-}
-if(isset($_POST['add_to_cart_starters'])){
-
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-
-
-   $product_image = $_POST['product_image'];
-   $product_quantity = 1;
-
-   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE (name = '$product_name' AND table_number ='$name_')");
-
-   if(mysqli_num_rows($select_cart) > 0){
-      $message[] = 'product already added to cart';
-   }else{
-      $insert_product = mysqli_query($conn, "INSERT INTO `cart`(name, prise, image, quantity,table_number, is_confirmed) VALUES('$product_name', '$product_price', '$product_image', '$product_quantity','$name_',0)");
-      $message[] = 'product added to cart succesfully';
-   }
-
-}
-
+include 'connect.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>products</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Digital Restaurant Menu</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        body {
+            background-image: url(image/woodBackground.jpeg);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+            background-size: cover;
+            width: 98vw;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+        header {
+            /* background: linear-gradient(rgb(18 20 28), rgb(82 177 182)); */
+            background: #000046;  fallback for old browsers
+background: -webkit-linear-gradient(to bottom, #1CB5E0, #000046);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #1CB5E0, #000046); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            
+
+            color: white;
+            border: 10px double white;
+            border-radius: 10px;
+            margin: 10px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 5px;
+            font-family: cursive;
+            font-size: 17px;
+            font-weight: bolder;
+            text-align: center;
+        }
+
+        main {
+            background-color: rgba(174, 174, 174, 0.425);
+            color: whitesmoke;
+            font-family: cursive;
+            font-size: 25px;
+            font-weight: bold;
+            padding: 70px;
+            text-align: center;
+            border: 5px solid white;
+            border-radius: 15px;
+            margin: 10px;
+            margin-bottom: 20px;
+            height: 63vh;
+        }
+
+        #welcome {
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 4vh;
+            margin-bottom: 3vh;
+        }
+
+        #form {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            color: white;
+            font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+                "Lucida Sans", Arial, sans-serif;
+            font-size: 16px;
+            font-weight: 600;
+        }
+
+        input {
+            text-align: center;
+            width: 250px;
+            height: 50px;
+            color: black;
+            font-size: 14px;
+            background-color: white;
+            border: 5px double black;
+            border-radius: 10px;
+        }
+
+        input:hover {
+            background-color: rgb(230, 230, 230);
+        }
+
+        #button {
+            width: 160px;
+            height: 45px;
+            font-size: 17px;
+            font-weight: bold;
+            background-color: rgb(230, 230, 230);
+        }
+
+        #button:hover {
+            background-color: gray;
+            color: white;
+            cursor: pointer;
+            border: 5px double white;
+        }
+
+        #button:active {
+            background-color: white;
+            color: black;
+        }
+
+        footer {
+            overflow: hidden;
+            font-family: "Courier New", Courier, monospace;
+            font-weight: bold;
+            padding: 8px;
+            background-color: rgb(69, 69, 69);
+            color: white;
+            border: 4px solid white;
+            border-radius: 12px;
+            margin: 10px;
+            margin-bottom: 0px;
+            text-align: right;
+        }
+    </style>
 </head>
+
 <body>
-   
-<?php
+    <header>
+        <h1>Foodies</h1>
+    </header>
 
-if(isset($message)){
-   foreach($message as $message){
-      echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
-   };
-};
+    <main>
+        <section id="welcome">
+            Welcome to our digital restaurant menu!!!
+            <section id="form">
+                <form action="products.php" >
+                    <input type="number" name="nam" id="tableNumber" placeholder="ENTER TABLE NUMBER" min="1"
+                        max="10" required />
+                    <br /><br />
+                    <input type="submit" value="SUBMIT" id="button" />
+                </form>
+                <form action="user_bag.php" method="post">
+                
+                </form>
+                <input type="hidden" name="nam" id="tableNumber" placeholder="ENTER TABLE NUMBER" min="1"
+                        max="10" required />
+            </div>
+            </section>
+    </main>
 
-?>
-
-<?php include 'header.php'; ?>
-
-
-<div class="container">
-
-<section class="products">
-
-   <h1 class="heading">Starters</h1>
-
-   
-    <?php
-   // $name__=$_SESSION["nam"];
-   // echo 'welcome'.$name__;
-   // echo $_GET['nam'];
-   ?> 
-
-
-   <div class="box-container">
-
-      <?php
-      
-      $select_products = mysqli_query($conn, "SELECT * FROM `starters`");
-      if(mysqli_num_rows($select_products) > 0){
-         while($fetch_product = mysqli_fetch_assoc($select_products)){
-      ?>
-
-      <form action="" method="post" class="hover">
-         <div class="box">
-            <img src="image/<?php echo $fetch_product['image']; ?>" alt="" width="280px" height="200px" >
-            <h3><?php echo $fetch_product['name']; ?></h3>
-            <div class="price">$<?php echo $fetch_product['prise']; ?>/-</div>
-            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
-            <input type="hidden" name="product_price" value="<?php echo $fetch_product['prise']; ?>">
-            <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
-            <input type="submit" class="btn" value="add to cart" name="add_to_cart_starters">
-         </div>
-      </form>
-
-      <?php
-         };
-      };
-      ?>
-
-   </div>
-
-</section>
-
-</div>
-<div class="container">
-
-<section class="products">
-
-   <h1 class="heading">Main Course</h1>
-
-   <div class="box-container">
-
-      <?php
-      
-      $select_products = mysqli_query($conn, "SELECT * FROM `main_course`");
-      if(mysqli_num_rows($select_products) > 0){
-         while($fetch_product = mysqli_fetch_assoc($select_products)){
-      ?>
-
-      <form action="" method="post">
-         <div class="box">
-            <img src="image/<?php echo $fetch_product['image']; ?>" alt="" width="280px" height="200px">
-            <h3><?php echo $fetch_product['name']; ?></h3>
-            <div class="price">$<?php echo $fetch_product['prise']; ?>/-</div>
-            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
-            <input type="hidden" name="product_price" value="<?php echo $fetch_product['prise']; ?>">
-            <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
-            <input type="submit" class="btn" value="add to cart" name="add_to_cart_Main_Course">
-         </div>
-      </form>
-
-      <?php
-         };
-      };
-      ?>
-
-   </div>
-
-</section>
-
-
-
-</div>
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-
+    <footer>Copyright @ 2022</footer>
 </body>
+
 </html>
